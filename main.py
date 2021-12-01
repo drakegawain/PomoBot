f031abf61d53513839b1faeefe0d3fbccc16081a
drakegawain <87667894+drakegawain@users.noreply.github.com>
2021-12-01 21:00:04 +0000

    esfdsfe

--- main.py
+++ main.py
@@ -13,7 +13,22 @@ async def on_message(message):
     return
 
   if message.content.startswith('.commands'):
-    await message.channel.send('.commands - Show the avaible commands /n')
+    await message.channel.send('\nEvery command that need inputs will be interpreted by the bot in minutes.\n.commands - Show the avaible commands \n.pomodoro - Starts a pomodoro counter, example: .pomodoro 25 15\n .regressive -Starts a regressive counter, example: .regressive 10')
+
+  if message.content.startswith('.pomodoro'):
+    author_name=message.author.VoiceChannel;
+    voice_channel = discord.VoiceChannel
+    pomodoro = message.content;
+    first_time = '';
+    second_time = '';
+    for char in pomodoro[9:12:1]:
+      first_time = first_time + char;
+    for char in pomodoro[12:15:1]:
+      second_time = second_time + char;
+  first_time = int(first_time)
+  second_time = int(second_time)
+  print(author_name);
+
 
 client.run(os.environ['TOKEN'])
 
