48d51c4a2a56dc218f6b4bcf5bfec35119172e14
drakegawain <87667894+drakegawain@users.noreply.github.com>
2021-12-05 23:15:19 +0000

    Bot can save ids tha joined session

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from threading import Timer
 
 client = discord.Client()
 c = 0;
+pomodoro_started = False;
+ids_get = [];
+
 @client.event
 async def on_ready():
   print('Logged in as {0.user}' .format(client))
@@ -14,11 +17,13 @@ async def on_message(message):
     return
 
   if message.content.startswith('.commands'):
-    await message.channel.send('\nEvery command that need inputs will be interpreted by the bot as minutes.\n.commands - Show the avaible commands \n.pomodoro - Starts a pomodoro counter, example: .pomodoro 25 15\n .regressive -Starts a regressive counter, example: .regressive 10')
+    await message.channel.send('\n<@%s>Every command that need inputs will be interpreted by the bot as minutes.\n.commands - Show the avaible commands \n.pomodoro - Starts a pomodoro counter, example: .pomodoro 25 15\n .regressive -Starts a regressive counter, example: .regressive 10' % message.author.id)
+    return
 
   if message.content.startswith('.pomodoro'):
-    
     async def join(message):
+      global pomodoro_started
+      pomodoro_started = True
       local = message.author.voice.channel;
       #guild = message.guild.id server ID
       await local.connect();
@@ -44,21 +49,31 @@ async def on_message(message):
     first_time = int(first_time)
     second_time = int(second_time)
     await join(message);
+    
     #colocar o timer de 30 segundos
     #depois dos 30 segundos, fechar a funcao join_pomodoro e mutar todos
 
+  if message.content.startswith('.join'):
+    async def join_pomodoro(message):
+     if pomodoro_started == True:
+        await message.channel.send('\n<@%s> Joined pomodoro' % message.author.id)
+        global c
+        c = c + 1;
+        return
 
-  
+    async def get_ids(message):
+      if pomodoro_started == False:
+          await message.channel.send('\n<@%s> No pomodoro was started. Type .pomodoro XX XX (where XX is time in minutes) to start pomodoro and then type .join.' % message.author.id)
+          return
+      else:
+          await join_pomodoro(message)
+          global c
+          global ids_get
+          ids_get.append((c - 1))
+          ids_get[(c - 1)] = message.author.id;
+          return
 
-  @client.event
-  async def join_pomodoro(message):
-    if message.content.startswith('.join'):
-      await message.channel.send('\n<@%s> Joined pomodoro' % message.author.id)
-      
-  async def conta_funcao():
-    await join_pomodoro(message)
-    global c
-    c = c+1
-  await conta_funcao()
+    await get_ids(message)
+
 client.run(os.environ['TOKEN'])
 
