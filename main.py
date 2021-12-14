8506f5fa202411bef2300c65e33121f39b4403b2
drakegawain <87667894+drakegawain@users.noreply.github.com>
2021-12-14 13:17:33 +0000

    Done with the short functions

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ pomodoro_started = False;
 ids_get = [];
 joined = 0;
 ids = set();
-study_time = 0;
-rest_time = 0;
+study_time_global = 0;
+rest_time_global = 0;
 
 #------------------FUNCTIONS-------------------
 
@@ -55,6 +55,60 @@ async def bot_id():
 async def list_keys(keys):
   keys_list = list(keys);
   return keys_list;
+
+async def check_ids(id):
+  global c, ids_get;
+  x = ids_get.index(id);
+  if len(ids_get) == 1:
+    return
+  if len(ids_get) != 1:
+    for y in range((x+1), len(ids_get)):
+      if ids_get[y] == id:
+        print('Already joined')
+        ids_get.pop(y);
+        c = len(ids_get);
+
+async def handle_joined(message):
+  global joined;
+  global ids_get;
+  if ids_get.count(message.author.id) == 1:
+    joined = 1;
+    return
+  if ids_get.count(message.author.id) > 1:
+    joined = 2;
+    return
+
+async def joined_function(message):
+  global joined;
+  await handle_joined(message);
+  if joined == 1:
+    return 'Joined'
+  if joined == 2:
+    return 'Already Joined'
+
+async def handle_c():
+  global c;
+  global pomodoro_started;
+  if pomodoro_started == True:
+    c = c + 1;
+
+async def get_ids(message):
+      if pomodoro_started == False:
+          await message.channel.send('\n<@%s> Pomodoro wasnt started. Type .pomodoro XX XX (where XX is time in minutes) to start pomodoro and then type .join.' % message.author.id)
+          return
+      else:
+          await handle_c();
+          global c
+          global ids_get
+          global ids
+          ids_get.append((c - 1))
+          ids_get[(c - 1)] = message.author.id;
+          #await check_ids(message.author.id);
+          total_ids = set(ids_get)
+          ids = total_ids;
+          print(ids);
+          await join_pomodoro(message)
+          return
 
 #------------------Mute/unmute-----------------
 
@@ -106,10 +160,10 @@ async def handle_rest_time(rest_time):
 
 async def study_time(message):
   pomodoro = message.content;
-  study_time = ''
+  time_study = ''
   for char in pomodoro[9:12:1]:
-    study_time = study_time + char;
-  return int(study_time)
+    time_study = time_study + char;
+  return int(time_study);
 
 async def rest_time(message):
   pomodoro = message.content;
@@ -124,6 +178,12 @@ async def connect_to_voice_channel(message):
   channel = message.author.voice.channel;
   await channel.connect()
   return
+
+async def join_pomodoro(message):
+  if pomodoro_started == True:
+    x = await joined_function(message)
+    await message.channel.send('\n<@%s> %s' % (message.author.id,x))
+    return
 
 #-------------------EVENTS--------------------
 
@@ -142,106 +202,21 @@ async def on_message(message):
 
   if message.content.startswith('.pomodoro'):
 
-    async def join(message):
-      global pomodoro_started
-      pomodoro_started = True
-      local = message.author.voice.channel;
-      #guild = message.guild.id server ID
-      await local.connect();
-      channel = client.get_channel(local.id)
-      keys = channel.voice_states.keys()
-      list_keys = list(keys)
-      #member_test = await client.fetch_user(list_keys[0]) members users
-      i = len(list_keys)
-      ids_mention = [];
-      bot_id = client.user.id;
-      for index in range(i):
-        if list_keys[index] != bot_id:
-          ids_mention.append(index);
-          ids_mention[index] = '<@%s>' % list_keys[index]
-      await message.channel.send('\nPomodoro starts in 30 seconds. The avaible users are:\n%s \nType .join to join pomodoro. ' % ids_mention)
-    pomodoro = message.content;
-    first_time = '';
-    second_time = '';
-    for char in pomodoro[9:12:1]:
-      first_time = first_time + char;
-    for char in pomodoro[12:15:1]:
-      second_time = second_time + char;
-    first_time = int(first_time)
-    second_time = int(second_time)
-    global study_time, rest_time;
-    study_time = await handle_study_time(first_time)
-    rest_time = await handle_rest_time(second_time)
-    print(study_time)
-    print(rest_time)
-    await join(message);
-    
+    await start_pomodoro();
+
+    await connect_to_voice_channel(message);
+
+    await message_avaiable_users_to_join(message, await avaiable_users_to_join(await list_keys(await get_keys(message)), await bot_id())); 
+
+    global study_time_global, rest_time_global;
+    study_time_global = await handle_study_time(await study_time(message));
+
+    rest_time_global = await handle_rest_time(await rest_time(message));
+
     #colocar o timer de 30 segundos
     #depois dos 30 segundos, fechar a funcao join_pomodoro e mutar todos
 
   if message.content.startswith('.join'):
-
-    async def handle_joined(message):
-      global joined;
-      global ids_get;
-      if ids_get.count(message.author.id) == 1:
-        joined = 1;
-        return
-      if ids_get.count(message.author.id) > 1:
-        joined = 2;
-        return
-
-    async def joined_function(message):
-      global joined;
-      await handle_joined(message);
-      if joined == 1:
-        return 'Joined'
-      if joined == 2:
-        return 'Already Joined'
-
-    async def handle_c():
-      global c;
-      global pomodoro_started;
-      if pomodoro_started == True:
-        c = c + 1;
-
-    async def join_pomodoro(message):
-      
-     if pomodoro_started == True:
-        x = await joined_function(message)
-        await message.channel.send('\n<@%s> %s' % (message.author.id,x))
-        return
-
-    async def check_ids(id):
-      global c, ids_get;
-      x = ids_get.index(id);
-      if len(ids_get) == 1:
-        return
-      if len(ids_get) != 1:
-        for y in range((x+1), len(ids_get)):
-          if ids_get[y] == id:
-            print('Already joined')
-            ids_get.pop(y);
-            c = len(ids_get);
-
-    async def get_ids(message):
-      if pomodoro_started == False:
-          await message.channel.send('\n<@%s> Pomodoro wasnt started. Type .pomodoro XX XX (where XX is time in minutes) to start pomodoro and then type .join.' % message.author.id)
-          return
-      else:
-          await handle_c();
-          global c
-          global ids_get
-          global ids
-          ids_get.append((c - 1))
-          ids_get[(c - 1)] = message.author.id;
-          #await check_ids(message.author.id);
-          total_ids = set(ids_get)
-          ids = total_ids;
-          print(ids);
-          await join_pomodoro(message)
-          return 
-  
     await get_ids(message)
 
   if message.content.startswith('.mute'):
