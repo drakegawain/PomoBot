8134f1bca95f8aac7dd153174135213f438ae2d8
drakegawain <87667894+drakegawain@users.noreply.github.com>
2022-03-08 16:22:55 +0000

    Pomojoin bug fixed

--- Commands/pomojoin.py
+++ Commands/pomojoin.py
@@ -4,8 +4,9 @@ from Pomodoro.Session_Handlers.get_session import get_session_pomojoin, OutsideV
 from Handle_Variables.handle_variables import get_ids
 #--------------------------------------------
 
-async def command_pomojoin(message):
-  session = await get_session(message, cfg.session_guilds)
+async def command_pomojoin(message, list_of_guilds:list):
+  index = await get_session(message, cfg.session_guilds)
+  session=cfg.session_guilds[index]
   if not hasattr(message.author.voice, 'channel'):
     raise OutsideVoiceChannel_pjoin(message)
   cur_vchan_session=await get_session_pomojoin(message, message.author.voice.channel, session)
